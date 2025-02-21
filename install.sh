85f5baddb1ebae8305084fd1360facd588c7f672
Luis <luiscristiangrigore11@gmail.com>
2025-02-21 17:05:16 +0100

    aaewrwerwgtgergerd

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 NAME="lcgg"
 
-INSTALL_DIR="/usr/local/bin/$($NAME)_tool"
+INSTALL_DIR="/usr/local/bin/$(echo $NAME)_tool"
 BIN_PATH="/usr/local/bin"
 
 echo "Instalando mi herramienta en $INSTALL_DIR..."
@@ -11,9 +11,9 @@ sudo git clone https://github.com/LuisGrigore/lcgg.git $INSTALL_DIR
 #sudo cp -r ./lcgg "$INSTALL_DIR"
 
 # Crear un enlace simbólico en /usr/local/bin para facilitar la ejecución
-sudo ln -sf "$INSTALL_DIR/$($NAME).sh" "$BIN_PATH/$($NAME)"
+sudo ln -sf "$INSTALL_DIR/$(echo $NAME).sh" "$BIN_PATH/$(echo $NAME)"
 
 # Dar permisos de ejecución
 sudo chmod +x "$INSTALL_DIR"
 
-echo "Instalation compleated. Now you can use '$($NAME)'."
+echo "Instalation compleated. Now you can use '$(echo $NAME)'."
