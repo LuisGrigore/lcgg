6ca4550ed4f997ceba4581d4d42edd2bd2c72f85
Luis <luiscristiangrigore11@gmail.com>
2025-02-21 14:53:41 +0100

    Primera versión de mi herramienta

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e  # Detiene el script en caso de error
+
+# Definir variables
+REPO_URL="https://github.com/LuisGrigore/lcgg.git"
+INSTALL_DIR="/usr/local/bin"
+BIN_NAME="lcgg"
+
+echo "Instalando $BIN_NAME..."
+
+# Descargar el último código desde GitHub
+if [ -d "$BIN_NAME" ]; then
+    rm -rf "$BIN_NAME"
+fi
+
+git clone "$REPO_URL" "$BIN_NAME"
+cd "$BIN_NAME"
+
+# Copiar el ejecutable al directorio de binarios del sistema
+chmod +x "$BIN_NAME"
+sudo mv "$BIN_NAME" "$INSTALL_DIR/"
+
+echo "Instalación completada. Ahora puedes ejecutar '$BIN_NAME' desde cualquier lugar."
