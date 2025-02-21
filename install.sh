5c7d7bdd9272366925c901dbe2ab33764b6d5f82
Luis <luiscristiangrigore11@gmail.com>
2025-02-21 15:34:02 +0100

    dfsdf

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ if [ -d "$BIN_DIR" ]; then
 fi
 
 git clone "$REPO_URL" "$BIN_NAME"
-cd "$APP_DIR"
+cd "lcgg/$APP_DIR"
 
 # Copiar el ejecutable al directorio de binarios del sistema
 chmod +x "$BIN_NAME"
