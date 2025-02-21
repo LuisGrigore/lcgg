412eb3838b96628a8c2e35b8996ea8e210f1e59f
Luis <luiscristiangrigore11@gmail.com>
2025-02-21 16:36:06 +0100

    dfsdf

--- install.sh
+++ install.sh
@@ -1,1 +1,17 @@
-pwd
+INSTALL_DIR="/usr/local/bin/lcgg"
+BIN_PATH="/usr/local/bin"
+
+echo "Instalando mi herramienta en $INSTALL_DIR..."
+
+# Crear directorio y copiar archivos
+sudo mkdir -p "$INSTALL_DIR"
+sudo git clone https://github.com/LuisGrigore/lcgg.git $INSTALL_DIR
+#sudo cp -r ./lcgg "$INSTALL_DIR"
+
+# Crear un enlace simbólico en /usr/local/bin para facilitar la ejecución
+sudo ln -sf "$INSTALL_DIR/lcgg" "$BIN_PATH"
+
+# Dar permisos de ejecución
+sudo chmod +x "$INSTALL_DIR"
+
+echo "Instalación completada. Ahora puedes ejecutar 'mi-herramienta.sh' desde cualquier lugar."
