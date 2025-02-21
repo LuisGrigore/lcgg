bb893ef1712c559e607c7e6da6d3f855288d4b4b
Luis <luiscristiangrigore11@gmail.com>
2025-02-21 16:22:45 +0100

    dfsdf

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo "Instalando mi herramienta en $INSTALL_DIR..."
 
 # Crear directorio y copiar archivos
 sudo mkdir -p "$INSTALL_DIR"
-sudo cp -r ./* "$INSTALL_DIR"
+sudo cp -r ./lcgg "$INSTALL_DIR"
 
 # Crear un enlace simbólico en /usr/local/bin para facilitar la ejecución
 sudo ln -sf "$INSTALL_DIR/mi-herramienta.sh" "$BIN_PATH"
