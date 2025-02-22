5acd8c88018c91e5d05bdccb593259d0bb2a9e37
Luis <luiscristiangrigore11@gmail.com>
2025-02-22 16:48:32 +0100

    aaa

--- lcgg.sh
+++ lcgg.sh
@@ -110,6 +110,11 @@ make_tests()
     make -f "$APP_DIR/test_make"
 }
 
+clean_tests()
+{
+    make -f "$APP_DIR/test_make" clean
+}
+
 #Command functions.
 update_command()
 {
@@ -120,6 +125,7 @@ test_command() {
     check_dir
     echo "$MSG_TESTING"
     make_tests
+	clean_tests
 }
 
 build_command() {
